ddbe47128326a10cd3bd3b20afc5928e440cacca
Wyatt Phillips <datscigye@gmail.com>
2019-05-21 15:45:38 -0400

    Add static site placeholder

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Host static content for www
+app.use(express.static('www'));
+
 app.get('/', (req,res) => {
     res.send('<h1>Welcome to this server!</h1>');
 });
