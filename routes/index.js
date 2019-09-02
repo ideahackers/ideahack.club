fcdef7ac287d703d29e0fb5cbebf979b20141649
Wyatt Phillips <datscigye@gmail.com>
2019-09-02 17:17:06 -0400

    Add carousel image passing

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,25 @@ router.use(Sentry.Handlers.requestHandler());
 // Home Page Route
 router.get('/', function rootHandler(req, res) {
 
-    res.render('index');
+    res.render('index', {
+        // @todo Add CRUD for images
+        // @body Allow for admins to modify images.
+        slides: [
+            {
+                url: "https://res.cloudinary.com/idea-hackers/image/upload/v1567453128/homepage-carousel/bg03_to8ddo.png",
+                number: 0,
+                active: "active"
+            },
+            {
+                url: "https://res.cloudinary.com/idea-hackers/image/upload/v1567453128/homepage-carousel/bg01_ktwt3n.png",
+                number: 1
+            },
+            {
+                url: "https://res.cloudinary.com/idea-hackers/image/upload/v1567453128/homepage-carousel/bg02_zuqusw.png",
+                number: 2
+            }
+        ]
+    });
 
 });
 
